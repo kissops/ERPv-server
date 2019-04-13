a680f4e82e388287084a336341102402012f6aaa
Um9i <vulgarmessiah@gmail.com>
2019-04-13 13:44:21 +0100

    filter querysets for current views

--- purchase/views.py
+++ purchase/views.py
@@ -6,8 +6,10 @@ class DeliverySchedule(ListView):
     model = PurchaseOrder
     paginate_by = 10
     template_name = "purchase/delivery_schedule.html"
+    queryset = PurchaseOrder.objects.filter(complete=False)
 
 
 class DeliveryDetail(DetailView):
     model = PurchaseOrder
     template_name = "purchase/delivery_detail.html"
+    queryset = PurchaseOrder.objects.filter(complete=False)
--- sales/views.py
+++ sales/views.py
@@ -6,8 +6,10 @@ class ShipmentSchedule(ListView):
     model = SalesOrder
     paginate_by = 10
     template_name = "sales/shipment_schedule.html"
+    queryset = SalesOrder.objects.filter(complete=False)
 
 
 class ShipmentDetail(DetailView):
     model = SalesOrder
     template_name = "sales/shipment_detail.html"
+    queryset = SalesOrder.objects.filter(complete=False)
