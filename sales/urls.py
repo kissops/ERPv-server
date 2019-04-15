4062803cae270257a52de96b0f49dc31cc1b7ee6
Um9i <vulgarmessiah@gmail.com>
2019-04-15 22:24:06 +0100

    update customer templates

--- sales/urls.py
+++ sales/urls.py
@@ -3,6 +3,7 @@ from .views import (
     CustomerCreate,
     CustomerDetail,
     CustomerList,
+    SalesOrderCreate,
     ShipmentSchedule,
     ShipmentDetail,
 )
@@ -11,6 +12,11 @@ urlpatterns = [
     path("customers/", CustomerList.as_view(), name="customer_list"),
     path("customers/<int:pk>/", CustomerDetail.as_view(), name="customer_detail"),
     path("customers/create/", CustomerCreate.as_view(), name="customer_create"),
+    path(
+        "customers/<int:pk>/sales_order/create/",
+        SalesOrderCreate.as_view(),
+        name="sales_order_create",
+    ),
     path("shipments/", ShipmentSchedule.as_view(), name="shipment_schedule"),
     path("shipments/<int:pk>/", ShipmentDetail.as_view(), name="shipment_detail"),
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,25 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from .models import Customer, SalesOrder
+
+
+class SalesOrderCreate(CreateView):
+    model = SalesOrder
+    fields = ["customer"]
+
+    def get_context_data(self, **kwargs):
+        customer = get_object_or_404(Customer, pk=self.kwargs["pk"])
+        kwargs["customer"] = customer
+        return super().get_context_data(**kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        self.pk = get_object_or_404(Customer, pk=kwargs["pk"])
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        form.instance.pk = self.pk
+        return super().form_valid(form)
 
 
 class CustomerCreate(CreateView):
