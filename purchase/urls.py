6dc42164fd064bdd4e06c0e6b00b12e57c458b4b
Um9i <vulgarmessiah@gmail.com>
2019-04-15 01:21:36 +0100

    update supplier frontend

--- purchase/urls.py
+++ purchase/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     DeliverySchedule,
     DeliveryDetail,
+    PurchaseOrderCreate,
     SupplierCreate,
     SupplierDetail,
     SupplierList,
@@ -12,5 +13,10 @@ urlpatterns = [
     path("deliveries/<int:pk>/", DeliveryDetail.as_view(), name="delivery_detail"),
     path("suppliers/", SupplierList.as_view(), name="supplier_list"),
     path("suppliers/<int:pk>/", SupplierDetail.as_view(), name="supplier_detail"),
+    path(
+        "suppliers/<int:pk>/purchase_order/create/",
+        PurchaseOrderCreate.as_view(),
+        name="purchase_order_create",
+    ),
     path("suppliers/create/", SupplierCreate.as_view(), name="supplier_create"),
 ]
--- purchase/views.py
+++ purchase/views.py
@@ -1,6 +1,25 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from .models import Supplier, PurchaseOrder
+
+
+class PurchaseOrderCreate(CreateView):
+    model = PurchaseOrder
+    fields = ["supplier"]
+
+    def get_context_data(self, **kwargs):
+        supplier = get_object_or_404(Supplier, pk=self.kwargs["pk"])
+        kwargs["supplier"] = supplier
+        return super().get_context_data(**kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        self.pk = get_object_or_404(Supplier, pk=kwargs["pk"])
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        form.instance.pk = self.pk
+        return super().form_valid(form)
 
 
 class SupplierCreate(CreateView):
